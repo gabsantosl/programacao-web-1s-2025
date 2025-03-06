4604616bdaee86a6bb6687a00145e01f958ebab1
Gabriela Santos <gabsantosleite@gmail.com>
2025-03-06 20:42:48 -0300

    atividade1

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,19 @@
+let numA = 9;
+let numB = 4;
+let numC; //indefinido
+
+// operador +
+numC = numA + numB;
+console.log('Resultado da soma entre ' + numA + ' e ' + numB + ' é ' + numC + '.');
+
+// operador -
+numC = numA - numB;
+console.log('Resultado da diferença entre ' + numA + ' e ' + numB + ' é ' + numC + '.');
+
+// operador *
+numC = numA * numB;
+console.log('Resultado da multiplicação entre ' + numA + ' e ' + numB + ' é ' + numC + '.');
+
+// operador /
+numC = numA / numB;
+console.log('Resultado da divisão entre ' + numA + ' e ' + numB + ' é ' + numC + '.');
